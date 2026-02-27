681ebfd908252487e36bebe38b9aab770c6259b5
Michael Howe <michael.howecwb@gmail.com>
2026-02-26 21:19:40 -0300

    update: configurado Keystack, criada página de exibição de projetos, atualização no design e layout em geral.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), keystatic()]
+  integrations: [react(), keystatic()],
+  adapter: vercel()
 });
